b88cb82c3056139489c444223dc82670e8ba9e60
Tyreece-Munusamy <tyreeceshivenm812@gmail.com>
2022-03-29 18:35:43 +0200

    Added Dexter to the project. Added camera/gallery permission dialog

--- app/src/main/java/com/example/happyplacesapp/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplacesapp/AddHappyPlaceActivity.kt
@@ -1,14 +1,28 @@
 package com.example.happyplacesapp
 
+import android.Manifest
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.Toolbar
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -37,7 +51,8 @@ class AddHappyPlaceActivity : AppCompatActivity() , View.OnClickListener {
                 updateDateInView()
             }
 
-        findViewById<androidx.appcompat.widget.AppCompatEditText>(R.id.et_date).setOnClickListener(this)
+        findViewById<AppCompatEditText>(R.id.et_date).setOnClickListener(this)
+        findViewById<TextView>(R.id.tv_add_image).setOnClickListener(this)
     }
 //boom
     override fun onClick(v: View?) {
@@ -49,6 +64,23 @@ class AddHappyPlaceActivity : AppCompatActivity() , View.OnClickListener {
                     , calendar.get(Calendar.MONTH)
                     , calendar.get(Calendar.DAY_OF_MONTH)).show()
             }
+
+            R.id.tv_add_image -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems =
+                    arrayOf("Select photo from gallery", "Capture photo from camera")
+                pictureDialog.setItems(
+                    pictureDialogItems
+                ) { dialog, which ->
+                    when (which) {
+                        // Here we have create the methods for image selection from GALLERY
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(this@AddHappyPlaceActivity,"Camera selection coming soon...", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                pictureDialog.show()
+            }
         }
     }
 
@@ -57,4 +89,50 @@ class AddHappyPlaceActivity : AppCompatActivity() , View.OnClickListener {
         val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
         findViewById<AppCompatEditText>(R.id.et_date).setText(sdf.format(calendar.time).toString())
     }
+
+    private fun choosePhotoFromGallery() {
+        // TODO(Step 6 : Asking the permissions of Storage using DEXTER Library which we have added in gradle file.)
+        // START
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()) {
+
+                        Toast.makeText(this@AddHappyPlaceActivity,"Storage READ/WRITE permission are granted. Now you can select an image from GALLERY or lets says phone storage.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread()
+            .check()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton("GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
 }
